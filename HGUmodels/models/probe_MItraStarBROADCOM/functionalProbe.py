a440cf4c98b2b831fa8ea8cce21dd90dea2e52d2
automacao <automacao@localhost.localdomain>
2022-05-20 16:49:21 -0300

    Add test 17 for MitraStar ECNT

--- HGUmodels/models/probe_MItraStarBROADCOM/functionalProbe.py
+++ HGUmodels/models/probe_MItraStarBROADCOM/functionalProbe.py
@@ -1,5 +1,6 @@
 import re
 import time
+import subprocess
 from datetime import datetime
 from ..MItraStarBROADCOM import HGU_MItraStarBROADCOM
 from json import JSONEncoder
@@ -29,6 +30,106 @@ config_collection = mongo_conn.get_collection()
 
 class HGU_MItraStarBROADCOM_functionalProbe(HGU_MItraStarBROADCOM):
 
+
+    # 17
+    def checkSpeedEthernetCable_17(self, flask_username):
+        """
+            Check the transmission speed on the ethernet network cable
+        :return : A dict with the result of the test
+        """
+        speed_test = "https://www.speedtest.net/"
+        # Verificar a velocidade contratada
+        down_speed_exp = 300
+        up_speed_exp = 300
+        try:
+            # Entering on WiFi 2.4GHz settings and sign in
+            self._driver.get('http://' + self._address_ip + '/')
+            time.sleep(1)
+            #config / Internet
+            self._driver.switch_to.frame("menufrm")
+            self._driver.find_element_by_xpath('/html/body/div/div/div/ul/li[2]/a').click()
+            time.sleep(1)
+            self._driver.find_element_by_xpath('/html/body/div/div/div/ul/li[2]/ul/li[3]/a').click()
+            time.sleep(2)
+            self._driver.switch_to.default_content()
+            self._driver.switch_to.frame('basefrm')
+            time.sleep(4)
+            self.admin_authentication_mitraStat()
+            time.sleep(2)
+            
+            # Desabling 2.4GHz WiFi
+            self._driver.find_element_by_xpath('/html/body/div/div/div[1]/div[3]/form/table/tbody/tr[1]/td[2]/input[2]').click()
+            self._driver.implicitly_wait(10)
+            self._driver.find_element_by_xpath('/html/body/div/div/div[1]/div[3]/form/table/tbody/tr[9]/td/a[2]/span').click()
+            time.sleep(3)
+            
+            # Desabling 5GHz WiFi
+            self._driver.switch_to.frame("menufrm")
+            try:
+                self._driver.find_element_by_xpath('/html/body/div/div/div/ul/li[2]/ul/li[4]/a').click()
+                time.sleep(2)
+            except Exception as e:
+                self._driver.find_element_by_xpath('/html/body/div/div/div/ul/li[2]/a').click()
+                time.sleep(2)
+                self._driver.find_element_by_xpath('/html/body/div/div/div/ul/li[2]/ul/li[4]/a').click()
+                time.sleep(2)
+            self._driver.switch_to.default_content()
+            self._driver.switch_to.frame('basefrm')
+            time.sleep(4)
+            self._driver.find_element_by_xpath('/html/body/div/div/div[1]/div[4]/form/table/tbody/tr[1]/td[2]/input[2]').click()
+            self._driver.find_element_by_xpath('/html/body/div/div/div[1]/div[4]/form/table/tbody/tr[9]/td/a[2]/span').click()
+            time.sleep(3)
+
+            self._driver.set_page_load_timeout(10)
+            try:
+                self._driver.execute_script("window.stop();")
+            except Exception as e:
+                print(e)
+                self._driver.get(speed_test)
+
+            # Desabling other devices
+            pwd = '4ut0m4c40'
+            cmd = 'ls'
+            subprocess.call('echo {} | sudo -S {}'.format(pwd, cmd), shell=True)
+
+            subprocess.run(['sudo', 'ifconfig', 'ens161', 'down'])
+            subprocess.run(['sudo', 'ifconfig', 'ens224', 'down'])
+            subprocess.run(['sudo', 'ifconfig', 'ens256', 'down'])
+
+            # Executing a Speed Test
+            try:
+                self._driver.get(speed_test)
+                self._driver.implicitly_wait(10)
+                time.sleep(10)
+                self._driver.find_element_by_xpath('//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[1]/a/span[4]').click()
+                time.sleep(60)
+                download_speed = float(self._driver.find_element_by_xpath('/html/body/div[3]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[2]/div/div[2]/span').text)
+                upload_speed = float(self._driver.find_element_by_xpath('/html/body/div[3]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[3]/div/div[2]/span').text)
+                print('#####################################################################')
+                print('Download Speed   -   ', download_speed)
+                print('Upload Speed     -   ', upload_speed)
+                print('#####################################################################')
+                if download_speed < 0.8*down_speed_exp:
+                    self._driver.quit()
+                    self._dict_result.update({"obs": 'A velocidade de Download está abaixo do esperado'})
+                elif upload_speed < 0.8*up_speed_exp:
+                    self._driver.quit()
+                    self._dict_result.update({"obs": 'A velocidade de Upload está abaixo do esperado'})
+                else:
+                    self._driver.quit()
+                    self._dict_result.update({"Resultado_Probe": "OK",'result':'passed', "obs": None})
+            except Exception as e:
+                print(e)
+
+        except Exception as exception:
+            print(exception)
+            self._driver.quit()
+            self._dict_result.update({"obs": str(exception)})
+        finally:
+            return self._dict_result
+
+
+    # 68
     def connectFakeWizard_68(self, flask_username):
 
         site1 = "http://{address_ip}/wancfg.cmd?action=view".format(address_ip=self._address_ip)
